ebeffcdc967b44d8b03b9b0ffd9afb3c0cac6583
sina <3inroydl@gmail.com>
2024-02-13 19:31:55 +0330

    add Doctor url

--- clinic/api/urls.py
+++ clinic/api/urls.py
@@ -5,4 +5,6 @@ from clinic.api import views
 urlpatterns = [
     path("testimonial/",views.TestimonialList.as_view(),name="testimonial-list"),
     path("testimonial/<int:pk>/", views.TestimonialRetrieve.as_view(), name="testimonial-retrieve"),
+    path("doctor/",views.DoctorList.as_view(),name="doctor-list"),
+    path("doctor/<int:pk>/", views.DoctorRetrieve.as_view(), name="doctor-retrieve"),
 ]
