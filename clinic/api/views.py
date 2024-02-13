0a379dbd127900d464c37bd1056dad24775a76b6
sina <3inroydl@gmail.com>
2024-02-13 18:39:17 +0330

    add Testimonial view

--- clinic/api/views.py
+++ clinic/api/views.py
@@ -2,5 +2,10 @@ from rest_framework import generics
 from clinic.api import serializers as ser
 
 
+class TestimonialList(generics.ListAPIView):
+    serializer_class = ser.TestimonialSerializer
 
+
+class TestimonialRetrieve(generics.RetrieveAPIView):
+    serializer_class = ser.TestimonialSerializer
 
